424ba2237d6b62ba80ccd50d9b4e29e87ccda151
Angel González <anngeeldavid@gmail.com>
2022-09-22 20:02:35 -0600

    Agregar archivos de proyecto.

---
+++ mult-constructor/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mult_constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            multiplicar MULT = new multiplicar();
+            MULT.V1 = double.Parse(textBox1.Text);
+            MULT.V2 = double.Parse(textBox2.Text);
+
+            textBox3.Text = MULT.multipica().ToString();
+        }
+    }
+}
---
+++ mult-constructor/multiplicar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mult_constructor
+{
+    class multiplicar
+    {
+        public double V1;
+        public double V2;
+
+        public multiplicar()
+        {
+        }
+
+        public multiplicar(double v1, double v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+        public double multipica()
+        {
+            return V1 * V2;
+        }
+    }
+}
